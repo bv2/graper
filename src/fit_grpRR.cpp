e741076f8f2a1e8ebf3fe644b38440dc81aca80e
Britta Velten <britta.velten@embl.de>
2017-10-31 11:42:23 -0700

    added random inits and removed defaults from all C functions and classes

--- src/fit_grpRR.cpp
+++ src/fit_grpRR.cpp
@@ -24,10 +24,10 @@ Rcpp::List grRRCpp_dense_nf(arma::mat X, arma::vec y, arma::Row<int> annot, int 
 
 //Fitting a normal prior model with fully factorized variational distribution
 // [[Rcpp::export]]
-Rcpp::List grRRCpp_dense_ff(arma::mat X, arma::vec y, arma::Row<int> annot, int g, arma::vec NoPerGroup, double d_tau =0.001, double r_tau =0.001,
-                            double d_gamma =0.001, double r_gamma =0.001, int max_iter=1000, double th=1e-7, bool calcELB=true, bool verbose=true, int freqELB=10){
+Rcpp::List grRRCpp_dense_ff(arma::mat X, arma::vec y, arma::Row<int> annot, int g, arma::vec NoPerGroup, double d_tau, double r_tau,
+                            double d_gamma, double r_gamma, int max_iter, double th, bool calcELB, bool verbose, int freqELB, arma::vec mu_init){
 
-  grpRR_dense_ff MyModel(X,y,annot,g,NoPerGroup, d_tau, r_tau, d_gamma, r_gamma, max_iter, th, calcELB, verbose, freqELB);
+  grpRR_dense_ff MyModel(X,y,annot,g,NoPerGroup, d_tau, r_tau, d_gamma, r_gamma, max_iter, th, calcELB, verbose, freqELB, mu_init);
   List result =MyModel.fitModel();
 
   return(result);
@@ -36,10 +36,11 @@ Rcpp::List grRRCpp_dense_ff(arma::mat X, arma::vec y, arma::Row<int> annot, int 
 
 //Fitting a spike and slab prior model with fully factorized variational distribution
 // [[Rcpp::export]]
-Rcpp::List grRRCpp_sparse_ff(arma::mat X, arma::vec y, arma::Row<int> annot, int g, arma::vec NoPerGroup, double d_tau =0.001, double r_tau =0.001,
-                          double d_gamma =0.001, double r_gamma =0.001, double r_pi=1, double d_pi=1, int max_iter=1000, double th=1e-7, bool calcELB=true, bool verbose=true, int freqELB=10){
+Rcpp::List grRRCpp_sparse_ff(arma::mat X, arma::vec y, arma::Row<int> annot, int g, arma::vec NoPerGroup, double d_tau, double r_tau,
+                          double d_gamma, double r_gamma, double r_pi, double d_pi, int max_iter, double th, bool calcELB, bool verbose,
+                             int freqELB, arma::vec mu_init, arma::vec psi_init){
 
-  grpRR_sparse_ff MyModel(X,y,annot,g,NoPerGroup, d_tau, r_tau, d_gamma, r_gamma, r_pi, d_pi, max_iter, th, calcELB, verbose, freqELB);
+  grpRR_sparse_ff MyModel(X,y,annot,g,NoPerGroup, d_tau, r_tau, d_gamma, r_gamma, r_pi, d_pi, max_iter, th, calcELB, verbose, freqELB, mu_init, psi_init);
   List result =MyModel.fitModel();
 
   return(result);
