c620670aac2afbbc4ed372c659bc676136e05155
Britta Velten <britta.velten@embl.de>
2017-11-06 10:43:39 -0800

    dded sparse logistic model

--- src/fit_grpRR.cpp
+++ src/fit_grpRR.cpp
@@ -75,10 +75,10 @@ Rcpp::List grpRRCpp_logistic_ff(arma::mat X, arma::vec y, arma::Row<int> annot, 
 //Fitting a spike and slab prior logistic model with fully factorized variational distribution
 // [[Rcpp::export]]
 Rcpp::List grpRRCpp_sparse_logistic_ff(arma::mat X, arma::vec y, arma::Row<int> annot, int g, arma::vec NoPerGroup,
-                                double d_gamma, double r_gamma, int max_iter, double th,
+                                double d_gamma, double r_gamma, double r_pi, double d_pi, int max_iter, double th,
                                 bool calcELB, bool verbose, int freqELB, arma::vec mu_init, arma::vec psi_init, bool intercept){
     
-    grpRR_sparse_logistic_ff MyModel(X,y,annot,g,NoPerGroup, d_gamma, r_gamma, max_iter, th, calcELB, verbose, freqELB, mu_init, psi_init,intercept);
+    grpRR_sparse_logistic_ff MyModel(X,y,annot,g,NoPerGroup, d_gamma, r_gamma, r_pi, d_pi, max_iter, th, calcELB, verbose, freqELB, mu_init, psi_init,intercept);
     List result =MyModel.fitModel();
     
     return(result);
