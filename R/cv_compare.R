4a4c3ead4ef9c7b4ac5332271eff9d93e436f0bb
Britta Velten <britta.velten@embl.de>
2017-11-15 13:22:52 +0100

    added parallel option

--- R/cv_compare.R
+++ R/cv_compare.R
@@ -13,20 +13,18 @@
 #' 
 #' @return List of fitted models and two data frames with coeffcients and penalty factors
 #' @import ggplot2 
-#' @import doParallel
+#' @import parallel
 #' @export
 
-cv_compare <- function(X, y, annot, family,ncores=1, nfolds=10, plot_cv=TRUE, seed=NULL,...){
+cv_compare <- function(X, y, annot, family, ncores=1, nfolds=10, plot_cv=TRUE, seed=NULL, parallel=FALSE,...){
   
   #make folds
   if(!is.null(seed)) set.seed(seed)
   foldid <- sample(rep(seq(nfolds), length=nrow(X)))
   
-  # register backend
-  # doParallel::registerDoParallel(cores=ncores)
-  
   # run methods and evaluate on left-out fold
-  resultList <- lapply(1:nfolds, function(foldidx){
+  if(parallel){
+  resultList <- parallel::mclapply(1:nfolds, function(foldidx){
     
     #split in train and test data
     use4test <- foldid==foldidx
@@ -53,20 +51,65 @@ cv_compare <- function(X, y, annot, family,ncores=1, nfolds=10, plot_cv=TRUE, se
     
     if (family=="gaussian"){
       RMSE <- getRMSE(AllFits)
-      list(FPR=FPR, FNR=FNR, RMSE=RMSE, pf_mat=pf_mat, beta_mat=beta_mat,
+      l <- list(FPR=FPR, FNR=FNR, RMSE=RMSE, pf_mat=pf_mat, beta_mat=beta_mat,
                          intercepts=intercepts, sparsity_mat=sparsity_mat, annot=AllFits$annot, runtime=runtime,
                          l1error_intercept, l1error_beta)
       } else if(family=="binomial"){
     BS <- getBS(AllFits)
     AUC <- getAUC(AllFits)
     ROC <- getROC(AllFits)
-    list(FPR=FPR, FNR=FNR, BS=BS, AUC=AUC, ROC=ROC, pf_mat=pf_mat, beta_mat=beta_mat,
+    l <- list(FPR=FPR, FNR=FNR, BS=BS, AUC=AUC, ROC=ROC, pf_mat=pf_mat, beta_mat=beta_mat,
                        intercepts=intercepts, sparsity_mat=sparsity_mat, annot=AllFits$annot, runtime=runtime,
                        l1error_intercept, l1error_beta)
       }
+    else stop("Family not implemented")
+  return(l)
 
-
-  })
+  }, mc.cores = ncores)
+  } else {
+    resultList <- lapply(1:nfolds, function(foldidx){
+      
+      #split in train and test data
+      use4test <- foldid==foldidx
+      ytrain <- y[ !use4test]
+      ytest <- y[use4test]
+      Xtrain <- X[ !use4test,]
+      Xtest <- X[use4test,]
+      
+      #fit models
+      AllFits <- RunMethods(Xtrain = as.matrix(Xtrain), ytrain =as.vector(ytrain),
+                            annot = annot, ...)
+      pf_mat <- getPenaltyFactors(AllFits)
+      sparsity_mat <- getSparsityLevel(AllFits)
+      beta_mat <- getCoefficients(AllFits)
+      intercepts <- getIntercept(AllFits)
+      
+      #evaluate prediciton performance
+      AllFits <- evaluateFits(AllFits, Xtest=as.matrix(Xtest), ytest=ytest)
+      runtime <- getRunTime(AllFits)
+      FNR <- getFNR(AllFits)
+      FPR <- getFPR(AllFits)
+      l1error_intercept <- getl1error_intercept(AllFits)
+      l1error_beta <- getl1error_beta(AllFits)
+      
+      if (family=="gaussian"){
+        RMSE <- getRMSE(AllFits)
+        l <- list(FPR=FPR, FNR=FNR, RMSE=RMSE, pf_mat=pf_mat, beta_mat=beta_mat,
+                  intercepts=intercepts, sparsity_mat=sparsity_mat, annot=AllFits$annot, runtime=runtime,
+                  l1error_intercept, l1error_beta)
+      } else if(family=="binomial"){
+        BS <- getBS(AllFits)
+        AUC <- getAUC(AllFits)
+        ROC <- getROC(AllFits)
+        l <- list(FPR=FPR, FNR=FNR, BS=BS, AUC=AUC, ROC=ROC, pf_mat=pf_mat, beta_mat=beta_mat,
+                  intercepts=intercepts, sparsity_mat=sparsity_mat, annot=AllFits$annot, runtime=runtime,
+                  l1error_intercept, l1error_beta)
+      }
+      else stop("Family not implemented")
+      return(l)
+      
+    })
+  }
   
   if(plot_cv) plotMethodComparison(resultList, family = family)
   
